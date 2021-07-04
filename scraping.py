51301471cee61d83b73a3d1e529f4da5bc86b85d
callistusikeata <82552594+callistusikeata@users.noreply.github.com>
2021-07-04 19:13:28 -0400

    Commit to main

--- scraping.py
+++ scraping.py
@@ -96,3 +96,8 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+if __name__ == "__main__":
+
+ # If running as script, print scraped data
+    print(scrape_all())
